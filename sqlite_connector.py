39241ec0289e6f4d351910271745b689e46e8df2
knawattranakul <knawattranakul@splunk.com>
2022-02-16 17:34:35 -0800

    fix insecure sql execute

--- sqlite_connector.py
+++ sqlite_connector.py
@@ -144,10 +144,8 @@ class SqliteConnector(BaseConnector):
         if len(results) < 1:
             return action_result.set_status(phantom.APP_ERROR, "There is no table with that name")
 
-        query = "PRAGMA table_info({});".format(table_name)
-
         try:
-            self._cursor.execute(query)
+            self._cursor.execute("PRAGMA table_info(?);", (table_name))
         except Exception as e:
             return action_result.set_status(
                 phantom.APP_ERROR, "Error listing columns", e
