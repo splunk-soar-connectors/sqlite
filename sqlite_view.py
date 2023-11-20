c8d0e8a7ffa2b9b1c94c6756ece7585bb9e38261
import-au <adam.bhdc@gmail.com>
2023-11-20 11:40:08 -0600

    Adding run_query view functionality.

--- sqlite_view.py
+++ sqlite_view.py
@@ -16,56 +16,32 @@ import json
 
 from django.http import HttpResponse
 
+def get_ctx_result(result):
+
+    ctx_result = {}
+    param = result.get_param()
+    summary = result.get_summary()
+    data = result.get_data()
+
+    ctx_result['param'] = param
+
+    if (data):
+        ctx_result['data'] = data
+        ctx_result['keys'] = list(dict(data[0]).keys())
+
+    if (summary):
+        ctx_result['summary'] = summary
+
+    return ctx_result
 
 def display_query_results(provides, all_results, context):
-
-    headers = []
-
-    headers_set = set()
+    context['results'] = results = []
     for summary, action_results in all_results:
         for result in action_results:
-            header_data = result.get_data()
 
-    if header_data:
-        headers += list(header_data[0].keys())
-
-    if not headers_set:
-        headers_set.update(headers)
-    headers = sorted(headers_set)
-
-    context['ajax'] = True
-    if 'start' not in context['QS']:
-        context['headers'] = headers
-        return '/widgets/generic_table.html'
-
-    adjusted_names = {}
-
-    start = int(context['QS']['start'][0])
-    length = int(context['QS'].get('length', ['5'])[0])
-    end = start + length
-    cur_pos = 0
-    rows = []
-    total = 0
-    for summary, action_results in all_results:
-        for result in action_results:
-            data = result.get_data()
-            total += len(data)
-            for item in data:
-                cur_pos += 1
-                if (cur_pos - 1) < start:
-                    continue
-                if (cur_pos - 1) >= end:
-                    break
-                row = []
-
-                for h in headers:
-                    row.append({ 'value': item.get(adjusted_names.get(h, h)) })
-                rows.append(row)
-
-    content = {
-        "data": rows,
-        "recordsTotal": total,
-        "recordsFiltered": total,
-    }
-
-    return HttpResponse(json.dumps(content), content_type='text/javascript')  # nosemgrep
+            ctx_result = get_ctx_result(result)
+            if (not ctx_result):
+                continue
+            results.append(ctx_result)
+    # print context
+    return 'sqlite.html'
